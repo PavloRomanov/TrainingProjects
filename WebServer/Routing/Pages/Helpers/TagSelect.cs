7bf368cd236734bc6a58b63c0d108631de174592
Serg <P.Serg76@mail.ru>
2016-01-19 22:37:25 +0200

    R

--- WebServer/Routing/Pages/Helpers/TagSelect.cs
+++ WebServer/Routing/Pages/Helpers/TagSelect.cs
@@ -9,20 +9,24 @@ namespace Routing.Pages.Helpers
 {
     public class TagSelect : HtmlTag
     {
+        private const string getName = "select";
         private string _name;
         private string _value;
         private MyList<string> _options;
-        public TagSelect(string getName, string text)
+        public TagSelect( string text)
             : base(getName, text)
         {
     
         }
-        public TagSelect(string getName, string text, string name, string value)
+        public TagSelect(string text, string name, string value)
            : base(getName, text)
         {
             _name = name;// name select
             _value = value;
-            _options = new MyList<string>();    
+            _options = new MyList<string>();
+            SetAdditionalOptions("1 years");
+            SetAdditionalOptions("3 years");
+            SetAdditionalOptions("5 years");
         }
        
         public TagSelect SetAdditionalOptions(string _value)
@@ -34,17 +38,16 @@ namespace Routing.Pages.Helpers
         {
             StringBuilder bodySelect = new StringBuilder();
             bodySelect.Append(Environment.NewLine);
-            bodySelect.Append("<").Append(Name).Append(" ").Append("name='").Append(_name).Append("'>");
-            bodySelect.Append("<option>");
-            SetAdditionalOptions("1 years");
-            SetAdditionalOptions("3 years");
-            SetAdditionalOptions("5 years");
+            bodySelect.Append("<").Append(Name).Append("  name='").Append(_name).Append("'>");
+         
             foreach (var option in _options)
             {
+                bodySelect.Append("<option>");
                 bodySelect.Append(option);
                 bodySelect.Append(Environment.NewLine);
+                bodySelect.Append("</option>");
             }
-            bodySelect.Append("</option>").Append("</").Append(Name).Append(">");
+            bodySelect.Append("</").Append(Name).Append(">");
             bodySelect.Append(Environment.NewLine);
             return bodySelect.ToString();
         }
