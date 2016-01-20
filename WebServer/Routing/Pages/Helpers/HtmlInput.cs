835df26e2201861d9a03b5e38d240851b38aa512
PavloRomanov <paul_r@ua.fm>
2016-01-20 22:13:57 +0200

    test

--- WebServer/Routing/Pages/Helpers/HtmlInput.cs
+++ WebServer/Routing/Pages/Helpers/HtmlInput.cs
@@ -15,10 +15,10 @@ namespace Routing.Pages.Helpers
         {
         }
 
-        public HtmlInput(string name, string value = null)
+        public HtmlInput(string name, string value2 = null)
         {
             _name = name;
-            _value = value;
+            _value = value2;
             _additionalAttributes = new MyHashTable<string, string>();
 
         }
