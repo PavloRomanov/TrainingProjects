8879dcc60fb9c7d7dd0930d596087d52167d7153
PavloRomanov <paul_r@ua.fm>
2016-03-15 20:08:29 +0200

    code first

--- WebShopMVC/WebShopMVC/Startup.cs
+++ WebShopMVC/WebShopMVC/Startup.cs
@@ -4,6 +4,7 @@ using Owin;
 [assembly: OwinStartupAttribute(typeof(WebShopMVC.Startup))]
 namespace WebShopMVC
 {
+    //https://msdn.microsoft.com/ru-ru/data/jj193542.aspx
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
