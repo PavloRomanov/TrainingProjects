803b6a17220d2395de5910f31938aefc565ac305
Lanasvet <sveta.boberskaya@gmail.com>
2016-03-17 16:49:10 +0200

    Add Product, Category, Subcategory

---
+++ WebShopMVC/Model/Entities/Subcategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model.Entities
+{
+    public class Subcategory
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SubcategoryId { get; set; }
+
+        [Required]
+        public string SubcategoryName { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
