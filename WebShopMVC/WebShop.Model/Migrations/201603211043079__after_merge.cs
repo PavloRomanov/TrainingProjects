2678d504ee614042a3dfabe80e9b2c111b1811dc
Sergey Konovalov <servako@gmail.com>
2016-03-22 12:40:18 +0200

    fix migration, do "Update-database -ProjectName WebShop.Model -Target 0", after that "Update-database -ProjectName WebShop.Model"

--- WebShopMVC/WebShop.Model/Migrations/201603211043079__after_merge.cs
+++ WebShopMVC/WebShop.Model/Migrations/201603211043079__after_merge.cs
@@ -7,7 +7,6 @@ namespace WebShop.Model.Migrations
     {
         public override void Up()
         {
-            AddColumn("dbo.Products", "Price", c => c.Decimal(nullable: false, precision: 18, scale: 2));
             AlterColumn("dbo.Products", "ProductName", c => c.String(nullable: false, maxLength: 30));
             AlterColumn("dbo.Products", "Description", c => c.String(maxLength: 200));
         }
@@ -16,7 +15,6 @@ namespace WebShop.Model.Migrations
         {
             AlterColumn("dbo.Products", "Description", c => c.String());
             AlterColumn("dbo.Products", "ProductName", c => c.String(nullable: false));
-            DropColumn("dbo.Products", "Price");
         }
     }
 }
