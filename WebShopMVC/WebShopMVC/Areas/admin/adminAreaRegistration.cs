3169a37af1bdab6d0f88380c9b3b32b1ae0c6c05
Serg <P.Serg76@mail.ru>
2016-03-29 19:04:40 +0300

    fix change Index

--- WebShopMVC/WebShopMVC/Areas/admin/adminAreaRegistration.cs
+++ WebShopMVC/WebShopMVC/Areas/admin/adminAreaRegistration.cs
@@ -17,8 +17,8 @@ namespace WebShopMVC.Areas.admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional },
-                new[] { "WebShopMVC.Areas.Admin.Controllers" }
+                new { action = "Index", id = UrlParameter.Optional }
+                ,new[] { "WebShopMVC.Areas.Admin.Controllers" }
             );
         }
     }
